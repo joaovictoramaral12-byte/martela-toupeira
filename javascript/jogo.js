1e9191b0c05e622dbbd8f42b1c15c428fed8d662
João Victor Amaral <joaovictoramaral12@gmail.com>
2025-11-16 14:38:46 -0300

    Decrease window duration for game response
    Reduced the window duration for the game from 200ms to 100ms.

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -2,7 +2,7 @@
 var acertos=0;
 var errados=0;
 var perdidos=0;
-var janela=200; //ms
+var janela=100; //ms
 var intervalo=500; //ms
 var timer = null;
 var jogoAtivo=false;
@@ -109,6 +109,7 @@ function restart(){
     location.reload();
 
 }
+
 
 
 
